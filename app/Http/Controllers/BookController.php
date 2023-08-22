01ce7c2ef6a91d8e5734c4fba60565d7abd50503
Pradeep <pradeepvjvargiya@gmail.com>
2023-08-22 17:44:57 +0530

    Popular or highest rated - the controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,13 +13,23 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');
 
         $books = Book::when(
             $title,
             fn($query, $title) => $query->title($title)
-        )
-            ->get();
-         return view('books.index', ['books' => $books]);
+        );
+
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+
+        $books = $books->get();
+        return view('books.index', ['books' => $books]);
     }
 
     /**
@@ -41,9 +51,16 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view(
+            'books.show',
+            [
+                'book' => $book->load([
+                    'reviews' => fn($query) => $query->latest()
+                ])
+            ]
+        );
     }
 
     /**
